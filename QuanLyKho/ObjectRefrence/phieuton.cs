9589f001f9fbeb56b93ef8295c80f63d422a3a77
hung_nguyen <hungnguyen1.bhtech@gmail.com>
2016-02-28 22:33:47 +0700

    no message

---
+++ QuanLyKho/ObjectRefrence/phieuton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyKho.ObjectRefrence
+{
+    class phieuton
+    {
+        public int Ma { get; set; }
+
+        public string Ten { get; set; }
+
+        public string DonViTinh { get; set; }
+
+        public Int64 SoLuong { get; set; }
+
+        public double Tien { get; set; }
+
+    }
+}

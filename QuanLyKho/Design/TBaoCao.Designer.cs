042fc29af70f79f929615090344bea37b5e8aef4
hung_nguyen <hungnguyen1.bhtech@gmail.com>
2016-02-29 16:22:16 +0700

    no message

--- QuanLyKho/Design/TBaoCao.Designer.cs
+++ QuanLyKho/Design/TBaoCao.Designer.cs
@@ -41,19 +41,22 @@
             // btIn
             // 
             this.btIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.btIn.Location = new System.Drawing.Point(506, 242);
+            this.btIn.Location = new System.Drawing.Point(675, 298);
+            this.btIn.Margin = new System.Windows.Forms.Padding(4);
             this.btIn.Name = "btIn";
-            this.btIn.Size = new System.Drawing.Size(106, 36);
+            this.btIn.Size = new System.Drawing.Size(141, 44);
             this.btIn.TabIndex = 113;
             this.btIn.Text = "In";
             this.btIn.UseVisualStyleBackColor = true;
+            this.btIn.Click += new System.EventHandler(this.btIn_Click);
             // 
             // btXem
             // 
             this.btXem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.btXem.Location = new System.Drawing.Point(384, 242);
+            this.btXem.Location = new System.Drawing.Point(512, 298);
+            this.btXem.Margin = new System.Windows.Forms.Padding(4);
             this.btXem.Name = "btXem";
-            this.btXem.Size = new System.Drawing.Size(106, 36);
+            this.btXem.Size = new System.Drawing.Size(141, 44);
             this.btXem.TabIndex = 112;
             this.btXem.Text = "Xem ";
             this.btXem.UseVisualStyleBackColor = true;
@@ -63,28 +66,36 @@
             // 
             this.cbBaoCao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.cbBaoCao.FormattingEnabled = true;
-            this.cbBaoCao.Location = new System.Drawing.Point(384, 147);
+            this.cbBaoCao.Items.AddRange(new object[] {
+            "báo cáo tồn vật tư",
+            "báo cáo xuất vật tư",
+            "báo cáo nhập vật tư"});
+            this.cbBaoCao.Location = new System.Drawing.Point(512, 181);
+            this.cbBaoCao.Margin = new System.Windows.Forms.Padding(4);
             this.cbBaoCao.Name = "cbBaoCao";
-            this.cbBaoCao.Size = new System.Drawing.Size(228, 28);
+            this.cbBaoCao.Size = new System.Drawing.Size(303, 33);
             this.cbBaoCao.TabIndex = 111;
+            this.cbBaoCao.SelectedIndexChanged += new System.EventHandler(this.cbBaoCao_SelectedIndexChanged);
             // 
             // tbDenNgay
             // 
             this.tbDenNgay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.tbDenNgay.Location = new System.Drawing.Point(525, 193);
+            this.tbDenNgay.Location = new System.Drawing.Point(700, 238);
+            this.tbDenNgay.Margin = new System.Windows.Forms.Padding(4);
             this.tbDenNgay.Mask = "00/00/0000";
             this.tbDenNgay.Name = "tbDenNgay";
-            this.tbDenNgay.Size = new System.Drawing.Size(87, 26);
+            this.tbDenNgay.Size = new System.Drawing.Size(115, 30);
             this.tbDenNgay.TabIndex = 110;
             this.tbDenNgay.ValidatingType = typeof(System.DateTime);
             // 
             // tbTuNgay
             // 
             this.tbTuNgay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.tbTuNgay.Location = new System.Drawing.Point(385, 193);
+            this.tbTuNgay.Location = new System.Drawing.Point(513, 238);
+            this.tbTuNgay.Margin = new System.Windows.Forms.Padding(4);
             this.tbTuNgay.Mask = "00/00/0000";
             this.tbTuNgay.Name = "tbTuNgay";
-            this.tbTuNgay.Size = new System.Drawing.Size(87, 26);
+            this.tbTuNgay.Size = new System.Drawing.Size(115, 30);
             this.tbTuNgay.TabIndex = 109;
             this.tbTuNgay.ValidatingType = typeof(System.DateTime);
             // 
@@ -92,9 +103,10 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label5.Location = new System.Drawing.Point(483, 196);
+            this.label5.Location = new System.Drawing.Point(644, 241);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(36, 20);
+            this.label5.Size = new System.Drawing.Size(45, 25);
             this.label5.TabIndex = 108;
             this.label5.Text = "đến";
             // 
@@ -102,9 +114,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label4.Location = new System.Drawing.Point(296, 196);
+            this.label4.Location = new System.Drawing.Point(395, 241);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(77, 20);
+            this.label4.Size = new System.Drawing.Size(100, 25);
             this.label4.TabIndex = 107;
             this.label4.Text = "Từ ngày : ";
             // 
@@ -112,15 +125,16 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.label1.Location = new System.Drawing.Point(282, 155);
+            this.label1.Location = new System.Drawing.Point(376, 191);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.Size = new System.Drawing.Size(138, 25);
             this.label1.TabIndex = 106;
-            this.label1.Text = "Tên vật tư : ";
+            this.label1.Text = "Tên báo cáo : ";
             // 
             // TBaoCao
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.btIn);
             this.Controls.Add(this.btXem);
@@ -130,8 +144,9 @@
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "TBaoCao";
-            this.Size = new System.Drawing.Size(894, 424);
+            this.Size = new System.Drawing.Size(1192, 522);
             this.Load += new System.EventHandler(this.TBaoCao_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
--- QuanLyKho/Design/TBaoCao.cs
+++ QuanLyKho/Design/TBaoCao.cs
@@ -26,5 +26,41 @@ namespace QuanLyKho.Design
         {
 
         }
+
+        private void btIn_Click(object sender, EventArgs e)
+        {
+            var value = cbBaoCao.SelectedIndex;
+
+            var from = new DateTime();
+            var to = new DateTime();
+            
+            switch (value)
+            {
+                case 0:
+                    QuanLyKho.BaoCao.nhapkho.baocaoton();
+                    break;
+                case 1:
+                    from = DateTime.Parse(tbTuNgay.Text);
+                    to = DateTime.Parse(tbDenNgay.Text);
+                    QuanLyKho.BaoCao.nhapkho.baocaonhap(from, to);
+                    break;
+                case 2:
+                    from = DateTime.Parse(tbTuNgay.Text);
+                    to = DateTime.Parse(tbDenNgay.Text);
+                    QuanLyKho.BaoCao.nhapkho.baocaoxuat(from, to);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void cbBaoCao_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var value = cbBaoCao.SelectedIndex;
+            if (value == 0)
+            {
+                
+            }
+        }
     }
 }
